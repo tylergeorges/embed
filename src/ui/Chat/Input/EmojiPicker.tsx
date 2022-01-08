b2a5028eea8ec3a3b5fe973ff291e43ba930b393
viction <mason@viction.dev>
2022-01-08 06:06:41 +0000

    feat: Category headers - sidebar left

--- src/ui/Chat/Input/EmojiPicker.tsx
+++ src/ui/Chat/Input/EmojiPicker.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from 'react';
 import { AutoSizer } from 'react-virtualized';
 import { FixedSizeList as List } from 'react-window';
-import { Root, Container, Sidebar, Content, EmojiDisplay, RowContainer } from './elements/emojipicker'
+import { Root, Container, Sidebar, Content, EmojiDisplay, RowContainer, NameDisplay, Icons } from './elements/emojipicker'
 import { Twemoji } from '@ui/shared/Emoji/emoji'
 import { defaultEmojis } from '@services/Emoji/defaultEmojis'
 import _ from 'lodash'
@@ -32,13 +32,17 @@ const EmojiPicker = ({ button, close, onSelect }: Props) => {
       ...obj,
       ...({ [emoji.category]: [...(obj[emoji.category] || []), emoji] })
     }), {});
-    Object.keys(categories).forEach(category => categories[category] = _.chunk(categories[category], rowWidth))
+
+    Object.keys(categories).forEach(category => categories[category] = _.chunk(categories[category], rowWidth));
+
     let index = 0
+
     Object.keys(categories).forEach(category => {
       categories[category] = [index, categories[category]]
       index += categories[category][1].length
       rowCount = index
     })
+
     return categories
   }, [defaultEmojis, rowWidth]) as Record<string, [number, any[][]]>
 
@@ -48,14 +52,23 @@ const EmojiPicker = ({ button, close, onSelect }: Props) => {
     <Root className="emoji-picker" innerRef={ref => picker = ref}>
       <Container>
         <Sidebar>
-          h
+          <div>
+            h
+          </div>
+          <div>
+            a
+          </div>
+          <div>
+            h
+          </div>
+          <div>
+            a
+          </div>
         </Sidebar>
 
         <Content>
           <AutoSizer>
             {({ width, height }) => {
-              console.log(`width: ${width}, height: ${height}`)
-
               if (!width || !height) return null
 
               setRowWidth(Math.floor((width - 10) / 40))
@@ -68,14 +81,31 @@ const EmojiPicker = ({ button, close, onSelect }: Props) => {
               >
                 {({ index, style }) => {
                   const [name, cat] = Object.entries(emojiCategories).reverse().find(c => c[1][0] <= index)
-                  const emojis = cat[1][index - cat[0]]
+
+                  if (index === cat[0]) {
+                    const CategoryIcon = Icons.mapped[name] || Icons.People;
+
+                    return (
+                      // @ts-expect-error
+                      <RowContainer style={style}>
+                        <NameDisplay>
+                          <CategoryIcon />
+                          {name}
+                        </NameDisplay>
+                      </RowContainer>
+                    )
+                  }
+
+                  const emojiIndex = index - 1;
+
+                  const emojis = cat[1][emojiIndex - cat[0]]
 
                   return (
                     // @ts-expect-error
                     <RowContainer style={style}>
                       <EmojiDisplay>
                         {emojis.map(emoji => (
-                          <EmojiDisplay onClick={() => onSelect(`:${emoji.keywords[0]}:`)}>
+                          <EmojiDisplay key={emoji.keywords[0]} onClick={() => onSelect(`:${emoji.keywords[0]}:`)}>
                             <Twemoji>{emoji.emoji}</Twemoji>
                           </EmojiDisplay>
                         ))}
