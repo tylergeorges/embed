faf0b18507afabc22a06fc395861740ce4df1715
advaith <advaithj1@gmail.com>
2022-04-20 15:33:30 -0700

    add tooltip to pin button

--- src/views/Messages/Header/Pins/index.tsx
+++ src/views/Messages/Header/Pins/index.tsx
@@ -5,6 +5,7 @@ import { observer } from 'mobx-react'
 import { useEffect, useState } from 'react'
 import { PinButton, Display, Title, List, Pin, NoPins } from './elements'
 import noPins from '@images/discordAssets/ef3a1ed683cfcf029971b12a26462072.svg'
+import Tooltip from 'rc-tooltip'
 
 export default observer(() => {
     const [open, setOpen] = useState(false)
@@ -38,7 +39,12 @@ export default observer(() => {
     const pins = generalStore.pins
 
     return <>
-        <PinButton innerRef={ref => (button = ref)} onClick={() => setOpen(!open)} open={open} className="pin-button" x="0" y="0" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 12L12.101 2.10101L10.686 3.51401L12.101 4.92901L7.15096 9.87801V9.88001L5.73596 8.46501L4.32196 9.88001L8.56496 14.122L2.90796 19.778L4.32196 21.192L9.97896 15.536L14.222 19.778L15.636 18.364L14.222 16.95L19.171 12H19.172L20.586 13.414L22 12Z"></path></PinButton>
+        <Tooltip
+            placement="bottom"
+            overlay="Pinned Messages"
+        >
+            <PinButton innerRef={ref => (button = ref)} onClick={() => setOpen(!open)} open={open} className="pin-button" x="0" y="0" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 12L12.101 2.10101L10.686 3.51401L12.101 4.92901L7.15096 9.87801V9.88001L5.73596 8.46501L4.32196 9.88001L8.56496 14.122L2.90796 19.778L4.32196 21.192L9.97896 15.536L14.222 19.778L15.636 18.364L14.222 16.95L19.171 12H19.172L20.586 13.414L22 12Z"></path></PinButton>
+        </Tooltip>
         {open && <Display right={right} innerRef={ref => (display = ref)} className="pin-display">
             <Title className="pin-title">Pinned Messages</Title>
             <List className="pin-list">
