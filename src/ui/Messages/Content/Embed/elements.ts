5be3beafa316f8feec15df5b541d1de6d79a1607
Tuur Martens <tuurmartens4@gmail.com>
2022-07-18 15:52:31 +0200

    embed support

--- src/ui/Messages/Content/Embed/elements.ts
+++ src/ui/Messages/Content/Embed/elements.ts
@@ -1,5 +1,146 @@
-import {css} from "react-emotion";
+import styled, {css} from "react-emotion";
 
 export const MediaEmbedBase = css`
   border-radius: 3px;
 `;
+
+export namespace EmbedStyle {
+  interface BaseProps {
+    color: string | undefined;
+  }
+
+  export const Base = styled.article<BaseProps>`
+    padding: 16px 16px 16px 12px;
+    border-left: 4px solid ${props => props.color ?? props.theme.colors._background.darken(0.7).string()};
+    background-color: ${props => props.theme.colors._background.darken(0.2).string()};
+    border-radius: 3px;
+    width: fit-content;
+    max-width: 520px;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  `;
+
+  interface ContentAndThumbnailProps {
+    thumbnailIsLarge: boolean;
+  }
+
+  export const ContentAndThumbnail = styled.div<ContentAndThumbnailProps>`
+    display: flex;
+    gap: 16px;
+    
+    ${props => props.thumbnailIsLarge && css`
+      flex-direction: column;
+      max-width: 432px;
+    `}
+  `;
+
+  export const Content = styled.div`
+    display: grid;
+    gap: 8px;
+  `;
+
+  export const Image = styled.img<{large?: boolean}>`
+    border-radius: 3px;
+    
+    ${props => props.large && css`
+      margin-top: 8px;
+    `}
+  `;
+
+  interface AuthorProps {
+    urlPresent: boolean;
+  }
+
+  export const Author = styled.div<AuthorProps>`
+    font-size: 14px;
+    font-weight: 600;
+    display: inline-grid;
+    grid-template-columns: auto 1fr;
+    align-items: center;
+    gap: 8px;
+    color: ${props => props.theme.colors._primary.string()};
+    
+    ${props => props.urlPresent && css`
+      &:hover {
+        cursor: pointer;
+        text-decoration: underline;
+      }
+    `}
+  `;
+
+  export const AuthorIcon = styled.img`
+    width: 24px;
+    height: 24px;
+    border-radius: 100%;
+  `;
+
+  export const AuthorName = styled.span`
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    display: inline-block;
+  `;
+
+  export const Title = styled.div`
+    font-size: 16px;
+    font-weight: 600;
+  `;
+
+  export const TitleWithUrl = styled.a`
+    font-size: 16px;
+    font-weight: 600;
+  `;
+
+  export const Description = styled.div`
+    font-size: 14px;
+  `;
+
+  export const Footer = styled.div`
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+  `;
+
+  export const FooterIcon = styled.img`
+    border-radius: 100%;
+    width: 20px;
+    height: 20px;
+    margin-right: 8px;
+  `;
+
+  export const Fields = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin-top: 4px;
+    gap: 8px;
+  `;
+
+  export const Field = styled.div<{inline?: boolean}>`
+    flex: 0;
+    max-width: 506px;
+    ${({inline}) =>
+      inline
+        ? css`
+          width: fit-content;
+          flex-basis: 30%;
+        `
+        : css`
+          flex-basis: 100%;
+        `
+    }
+  `;
+
+  export const FieldName = styled.div`
+    font-size: 14px;
+    margin-bottom: 4px;
+    font-weight: 600;
+    color: ${props => props.theme.colors._primary.string()};
+  `;
+
+  export const FieldValue = styled.div`
+    font-size: 14px;
+    font-weight: 400;
+  `;
+}
--- src/ui/Messages/Content/Embed/index.tsx
+++ src/ui/Messages/Content/Embed/index.tsx
@@ -2,6 +2,10 @@ import {Message_embeds} from "@generated";
 import GifVEmbed from "@ui/Messages/Content/Embed/GifVEmbed";
   import ImageEmbed from "@ui/Messages/Content/Embed/ImageEmbed";
 import VideoAttachment from "@ui/Messages/Content/Attachment/VideoAttachment";
+import {EmbedStyle} from "@ui/Messages/Content/Embed/elements";
+import numberToRgb from "@utils/numberToRgb";
+import {useMemo} from "react";
+import moment from "moment";
 
 export interface EmbedProps {
   embed: Message_embeds;
@@ -14,10 +18,118 @@ function Embed({embed}: EmbedProps) {
   if (embed.type.toLowerCase() === 'image')
     return <ImageEmbed embed={embed} />;
 
-  if (embed.type.toLowerCase() === 'video')
+  if (embed.type.toLowerCase() === 'video' && !embed.thumbnail)
     return <VideoAttachment attachmentOrEmbed={embed} />;
 
-  return null;
+  console.log("Embed", embed);
+
+  const embedColor = embed.color !== 0 && embed.color !== null
+    ? numberToRgb(embed.color)
+    : undefined;
+
+  const { width, height, isLarge } = useMemo(() => {
+    const image = embed.image ?? embed.thumbnail;
+
+    if (image === null)
+      return { width: null, height: null, isLarge: false };
+
+    if (/^article|image|rich$/i.test(embed.type)) {
+      const proposedWidth = 400;
+      const proposedHeight = proposedWidth / image.width * image.height;
+
+      const { width, height } = proposedHeight > proposedWidth
+        ? { width: 300 / image.height * image.width, height: 300 }
+        : { width: proposedWidth, height: proposedHeight };
+
+      return {
+        width,
+        height,
+        isLarge: true
+      };
+    }
+
+    const aspectRatio = image.width / image.height;
+    const imageHeight = 80 / aspectRatio;
+    const imageWidth = imageHeight / image.height * image.width;
+
+    return { width: imageWidth, height: imageHeight, isLarge: false };
+  }, [embed.type, embed.image, embed.thumbnail]);
+
+  // TODO: author url
+  // TODO: provider
+
+  return (
+    <EmbedStyle.Base color={embedColor}>
+      <EmbedStyle.ContentAndThumbnail thumbnailIsLarge={isLarge}>
+        <EmbedStyle.Content>
+          {embed.author && (
+            <EmbedStyle.Author urlPresent={embed.author.url !== null}>
+              {embed.author.icon && (
+                <EmbedStyle.AuthorIcon src={embed.author.icon} />
+              )}
+              <EmbedStyle.AuthorName>{embed.author.name}</EmbedStyle.AuthorName>
+            </EmbedStyle.Author>
+          )}
+          {embed.title && (
+            embed.url !== null
+              ? (
+                <EmbedStyle.TitleWithUrl href={embed.url} target="_blank">{embed.title}</EmbedStyle.TitleWithUrl>
+              )
+              : (
+                <EmbedStyle.Title>{embed.title}</EmbedStyle.Title>
+              )
+          )}
+          {embed.description && (
+            <EmbedStyle.Description>{embed.description}</EmbedStyle.Description>
+          )}
+          {embed.fields && embed.fields.length > 0 && (
+            <EmbedStyle.Fields>
+              {embed.fields.map(field => (
+                <EmbedStyle.Field
+                  key={field.name + field.value}
+                  inline={field.inline}
+                >
+                  <EmbedStyle.FieldName>
+                    {field.name}
+                  </EmbedStyle.FieldName>
+                  <EmbedStyle.FieldValue>
+                    {field.value}
+                  </EmbedStyle.FieldValue>
+                </EmbedStyle.Field>
+              ))}
+            </EmbedStyle.Fields>
+          )}
+        </EmbedStyle.Content>
+        {embed.thumbnail && (
+          <EmbedStyle.Image
+            src={embed.thumbnail.url}
+            width={width}
+            height={height}
+          />
+        )}
+      </EmbedStyle.ContentAndThumbnail>
+      {embed.image && (
+        <EmbedStyle.Image
+          src={embed.image.url}
+          width={width}
+          height={height}
+          large={true}
+        />
+      )}
+
+      {(embed.footer || embed.timestamp) && (
+        <EmbedStyle.Footer>
+          {embed.footer?.url && (
+            <EmbedStyle.FooterIcon src={embed.footer.url} />
+          )}
+          {embed.footer?.text}
+          {embed.timestamp && (
+              <>{" • "}{moment(embed.timestamp).calendar()}</>
+          )}
+        </EmbedStyle.Footer>
+      )}
+    </EmbedStyle.Base>
+  );
 }
 
 export default Embed;
