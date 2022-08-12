93579214d81c6e6ba793bcde75d7f5c9450a085e
Tuur Martens <tuurmartens4@gmail.com>
2022-08-12 15:19:12 +0200

    fix formatting to be consistent

--- src/ui/Sidebar/Chats/index.tsx
+++ src/ui/Sidebar/Chats/index.tsx
@@ -14,62 +14,61 @@ import { FaPlus } from "react-icons/fa";
 import { store } from "@models";
 
 export const ChatSwitcher = observer(() => {
-    if (!generalStore.chats)
-        client.query<Chats>({ query: CHATS, variables: { guild: generalStore.guild.id }, fetchPolicy: 'network-only' })
-            .then(({ data: { getChats: chats } }) => generalStore.setChats(chats))
+  if (!generalStore.chats)
+    client.query<Chats>({ query: CHATS, variables: { guild: generalStore.guild.id }, fetchPolicy: 'network-only' })
+      .then(({ data: { getChats: chats } }) => generalStore.setChats(chats))
 
-    const { guild, channel } = useRouter();
-    const navigate = useNavigate();
+  const { guild, channel } = useRouter();
+  const navigate = useNavigate();
 
-    if (!generalStore.chats) return <LoadingContainer><Loading /></LoadingContainer>
+  if (!generalStore.chats) return <LoadingContainer><Loading /></LoadingContainer>
 
-    const userId = channel?.startsWith('@') ? channel.substring(1) : null;
-    if (userId && !generalStore.chats.find(r => r.recipient.id === userId)) {
-        client.query<UserTag>({
-            query: USER_TAG,
-            variables: { guild, user: userId }
-        }).then(({ data: { userData }}) => {
-            if (!userData) return
+  const userId = channel?.startsWith('@') ? channel.substring(1) : null;
+  if (userId && !generalStore.chats.find(r => r.recipient.id === userId)) {
+    client.query<UserTag>({
+      query: USER_TAG,
+      variables: { guild, user: userId }
+    }).then(({ data: { userData }}) => {
+      if (!userData) return
 
-            // If an external ID was passed in, change url to the WidgetBot id
-            if (userId !== userData.id) navigate('@' + userData.id, { replace: true });
+      // If an external ID was passed in, change url to the WidgetBot id
+      if (userId !== userData.id) navigate('@' + userData.id, { replace: true });
 
-            if (generalStore.chats.find(r => r.recipient.id === userData.id)) return
+      if (generalStore.chats.find(r => r.recipient.id === userData.id)) return
 
-            generalStore.setChats([
-                {
-                    content: "",
-                    recipient: { ...userData, bot: false }
-                },
+      generalStore.setChats([
+        {
+          content: "",
+          recipient: { ...userData, bot: false }
+        },
 
-              ...generalStore.chats,
-            ]);
-        });
-    }
+        ...generalStore.chats,
+      ]);
+    });
+  }
 
-    return (
-        <Root className="channels">
-            {authStore.user && 'provider' in authStore.user && authStore.user.provider === 'Guild' &&
-                <NewChatButton onClick={store.modal.openNewChat}><FaPlus /> New Chat</NewChatButton>}
-            {generalStore.chats.map((chat) => (
-                <NavLink
-                    key={chat.recipient.id}
-                    to={`/channels/${guild}/@${chat.recipient.id}`}
-                    children={
-                        <Chat selected={channel === '@'+chat.recipient.id}>
-                            <Avatar width={32} height={32} src={chat.recipient.avatarUrl} />
-                            <Details>
-                                <Member color={chat.recipient.color}>{chat.recipient.name}{chat.recipient.discrim !== '0000' ? `#${chat.recipient.discrim}` : ''}</Member>
-                                <Preview>{chat.content}</Preview>
-                            </Details>
-                        </Chat>
-                    }
-                    style={{ textDecoration: 'none' }}
-                />
-            ))}
-        </Root>
-    )
-
+  return (
+    <Root className="channels">
+      {authStore.user && 'provider' in authStore.user && authStore.user.provider === 'Guild' &&
+        <NewChatButton onClick={store.modal.openNewChat}><FaPlus /> New Chat</NewChatButton>}
+      {generalStore.chats.map((chat) => (
+        <NavLink
+          key={chat.recipient.id}
+          to={`/channels/${guild}/@${chat.recipient.id}`}
+          children={
+            <Chat selected={channel === '@'+chat.recipient.id}>
+              <Avatar width={32} height={32} src={chat.recipient.avatarUrl} />
+              <Details>
+                <Member color={chat.recipient.color}>{chat.recipient.name}{chat.recipient.discrim !== '0000' ? `#${chat.recipient.discrim}` : ''}</Member>
+                <Preview>{chat.content}</Preview>
+              </Details>
+            </Chat>
+          }
+          style={{ textDecoration: 'none' }}
+        />
+      ))}
+    </Root>
+  );
 })
 
 export default ChatSwitcher
