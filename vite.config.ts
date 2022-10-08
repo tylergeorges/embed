192df6b50404663632e070e29ee6cf97816fb08c
Daave <root@widgetbot.io>
2022-10-08 13:26:45 +0100

    feat: ESLint & Prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin']
+      }
+    }),
+    eslint({ include: ['src/**/*.{ts,tsx}'] }),
+    tsconfigPaths()
+  ],
+  server: {
+    port: 3443,
+    strictPort: true,
+    open: true
+  }
+});
