7506a22dbb6421b461bc2f73d989c5325a78a955
Daave <root@widgetbot.io>
2022-10-08 14:42:11 +0100

    feat: Remove emotion

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,7 @@ import codegen from 'vite-plugin-graphql-codegen';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin']
-      }
-    }),
+    react(),
     eslint({
       include: ['src/**/*.{ts,tsx}'],
       failOnError: process.env.CI === 'true',
