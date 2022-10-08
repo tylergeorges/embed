5a1d7559c700acf49aaa3a398730f08e05d04aca
Mason Rogers <mason@viction.dev>
2022-10-08 01:45:19 +0100

    rq documentation on env shit

--- src/util/env.ts
+++ src/util/env.ts
@@ -1,3 +1,8 @@
+/*
+ * This file is for runtime variables which are set on the Docker container and replaced on boot.
+ * To add a new variable, add a key with an array value of ['{{KEY}}', 'default value']
+ * KEY **MUST** be the same as the actual object key.
+ */
 const VARIABLES: Record<string, [string, string]> = {
   CUSTOM_SERVER_ENDPOINT: ['{{CUSTOM_SERVER_ENDPOINT}}', 's-staging.widgetbot.io']
 };
