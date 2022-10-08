594b1214a29c36a704bc4835689ad857fade50b4
Mason Rogers <mason@viction.dev>
2022-10-08 01:18:34 +0100

    feat: runtime env

---
+++ src/util/env.ts
@@ -0,0 +1,12 @@
+const VARIABLES: Record<string, [string, string]> = {
+  CUSTOM_SERVER_ENDPOINT: ['{{CUSTOM_SERVER_ENDPOINT}}', 's-staging.widgetbot.io']
+};
+
+export function getEnvVar(key: string): string | undefined {
+  if (!VARIABLES[key]) return;
+
+  const [replaced, defaultValue] = VARIABLES[key];
+  if (replaced === `{{${key}}`) return defaultValue;
+
+  return replaced.substring(2, replaced.length - 1);
+}
