d0e4d7addbf167f84da9937c13ed33ee228c8de3
Mason Rogers <mason@viction.dev>
2023-04-30 21:19:32 +0100

    codegen

--- src/graphql/gql.ts
+++ src/graphql/gql.ts
@@ -3,14 +3,18 @@ import * as types from './graphql';
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
 const documents = {
-    "\n    query Guild($id: String!) {\n        guild(id: $id) {\n            id\n            name\n            settings {\n                readonly\n            }\n            channels {\n                id\n                name\n                type\n                position\n                category {\n                    id\n                    name\n                    position\n                }\n                rateLimitPerUser\n            }\n        }\n    }\n": types.GuildDocument,
+  '\n  query Guild($id: String!) {\n    guild(id: $id) {\n      id\n      name\n      settings {\n        readonly\n      }\n      channels {\n        id\n        name\n        type\n        position\n        category {\n          id\n          name\n          position\n        }\n        rateLimitPerUser\n      }\n    }\n  }\n':
+    types.GuildDocument
 };
 
-export function graphql(source: "\n    query Guild($id: String!) {\n        guild(id: $id) {\n            id\n            name\n            settings {\n                readonly\n            }\n            channels {\n                id\n                name\n                type\n                position\n                category {\n                    id\n                    name\n                    position\n                }\n                rateLimitPerUser\n            }\n        }\n    }\n"): (typeof documents)["\n    query Guild($id: String!) {\n        guild(id: $id) {\n            id\n            name\n            settings {\n                readonly\n            }\n            channels {\n                id\n                name\n                type\n                position\n                category {\n                    id\n                    name\n                    position\n                }\n                rateLimitPerUser\n            }\n        }\n    }\n"];
+export function graphql(
+  source: '\n  query Guild($id: String!) {\n    guild(id: $id) {\n      id\n      name\n      settings {\n        readonly\n      }\n      channels {\n        id\n        name\n        type\n        position\n        category {\n          id\n          name\n          position\n        }\n        rateLimitPerUser\n      }\n    }\n  }\n'
+): typeof documents['\n  query Guild($id: String!) {\n    guild(id: $id) {\n      id\n      name\n      settings {\n        readonly\n      }\n      channels {\n        id\n        name\n        type\n        position\n        category {\n          id\n          name\n          position\n        }\n        rateLimitPerUser\n      }\n    }\n  }\n'];
 
 export function graphql(source: string): unknown;
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
+  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
