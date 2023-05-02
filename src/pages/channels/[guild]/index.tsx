57814917750aee9199d83e1affb6cd61fd8b65fc
Daave <lewis@widgetbot.io>
2023-05-02 13:58:32 +0100

    fix: Remove SSR function

--- src/pages/channels/[guild]/index.tsx
+++ src/pages/channels/[guild]/index.tsx
@@ -2,12 +2,6 @@ import { getEnvVar } from '@util/env';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
-export async function getServerSideProps() {
-  return {
-    props: {} // will be passed to the page component as props
-  };
-}
-
 const GuildIndex: NextPage = () => {
   const router = useRouter();
   const { guild } = router.query;
