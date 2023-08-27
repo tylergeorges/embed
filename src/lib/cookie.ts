857bebf4268d35eb507cfa673bd7ed2ef4f7b725
Mason Rogers <mason@viction.dev>
2023-08-27 17:44:24 +0100

    getCookie helper func

---
+++ src/lib/cookie.ts
@@ -0,0 +1,8 @@
+export function getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+
+  return parts.length === 2
+    ? parts.pop().split(';').shift()
+    : null;
+}
