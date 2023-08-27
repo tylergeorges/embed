794e5c9bc1385e5801d5f225cd94578caa2f0f51
Mason Rogers <mason@viction.dev>
2023-08-27 17:44:42 +0100

    dont use ls to pass token to apollo

--- src/lib/apollo/index.ts
+++ src/lib/apollo/index.ts
@@ -7,17 +7,9 @@ import link from './link'
 const queryParams = new URLSearchParams(location.search)
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  let token: string
-  try {
-    token = window.localStorage.getItem('token');
-  } catch (e) {}
-
   headers ??= {}
 
-  // return the headers to the context so httpLink can read them
-  if (token) headers.authorization = token
-  if (queryParams.has('settings-group')) headers['X-Settings-Group'] = queryParams.get('settings-group')
+  if (queryParams.has('settings-group')) headers['X-Settings-Group'] = queryParams.get('settings-group');
 
   return { headers }
 });
