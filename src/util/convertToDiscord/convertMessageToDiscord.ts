8bbd6e43a8d3f2765b55e5f6c86eb573e2eb69fe
tylergeorges <tmg320v@gmail.com>
2023-08-28 16:48:19 -0400

    fix: added missing fields in convertMessageToDiscord

--- src/util/convertToDiscord/convertMessageToDiscord.ts
+++ src/util/convertToDiscord/convertMessageToDiscord.ts
@@ -1,29 +1,38 @@
-import { APIEmbed, MessageType } from 'discord-api-types/v10';
+import {
+  APIEmbed,
+  APIUser,
+  MessageType,
+  APIMessage,
+  InteractionType,
+  APIMessageInteraction
+} from 'discord-api-types/v10';
 import { Message } from '@graphql/graphql';
 import { getAvatarId } from '@util/convertToDiscord/getAvatarId';
-import { ExpandedAPIMessage } from 'types/messages.types';
 
-const getIdFromUrl = (message: Message) => {
-  const id = message.author.avatarUrl.includes('gravatar')
-    ? message.author.id
-    : message.author.avatarUrl.split('/')[4] ?? null;
+const getIdFromUrl = (avatarUrl: string) => {
+  const id = avatarUrl.split('/')[4] ?? null;
 
   return id;
 };
 
-export const convertMessageToDiscord = (message: Message): ExpandedAPIMessage => ({
+export const convertMessageToDiscord = (message: Message): APIMessage & { isGuest: boolean } => ({
   id: message.id,
   type: MessageType.Default,
   channel_id: message.channelId,
   content: message.content,
+
   // convert epoch to '2022-11-17T19:23:27.904000+00:00'
   timestamp: new Date(message?.createdAt)?.toISOString(),
+
   edited_timestamp: message.editedAt,
   flags: message.flags ?? 0,
   isGuest: message.isGuest,
 
   author: {
-    id: message.isGuest && message.author.bot ? getIdFromUrl(message) : message.author.id,
+    id:
+      message.isGuest && message.author.bot
+        ? getIdFromUrl(message.author.avatarUrl)
+        : message.author.id,
     bot: message.author.bot,
     username: message.author.name,
     avatar: getAvatarId(message.author.avatarUrl),
@@ -43,23 +52,68 @@ export const convertMessageToDiscord = (message: Message): ExpandedAPIMessage =>
     width: a.width
   })),
 
-  embeds: message.embeds.map(e => ({
-    author: e.author,
-    color: e.color,
-    description: e.description,
-    footer: e.footer,
-    image: e.image ?? {},
-    provider: e.provider,
-    thumbnail: e.thumbnail ?? {},
-    timestamp: e.timestamp,
-    title: e.title,
-    url: e.url,
-    fields: e.fields,
-    video: e.video ?? {}
-  })) as APIEmbed[],
+  embeds: message.embeds
+    ? (message.embeds.map(e => ({
+        author: e.author,
+        color: e.color,
+        description: e.description,
+        footer: e.footer,
+        image: e.image ?? {},
+        provider: e.provider,
+        thumbnail: e.thumbnail ?? {},
+        timestamp: e.timestamp,
+        title: e.title,
+        url: e.url,
+        fields: e.fields,
+        video: e.video ?? {}
+      })) as APIEmbed[])
+    : [],
 
-  reactions: [],
-  mentions: [],
+  reactions: message.reactions
+    ? message.reactions.map(r => ({
+        count: r.count as number,
+        emoji: {
+          id: r.emojiId as string,
+          name: r.emojiName as string,
+          animated: r.animated as boolean
+        },
+        me: r.me
+      }))
+    : [],
+
+  mentions: message.mentions
+    ? (message.mentions.map(m => ({
+        id: m.id,
+        guild_id: m.id,
+        type: m.type,
+        name: m.name,
+        global_name: m.name,
+        discriminator: '0000',
+        username: m.name,
+        avatar: ''
+      })) as APIUser[])
+    : [],
+
+  interaction: message.interaction
+    ? ({
+        id: message.interaction.id,
+        name: message.interaction.name,
+
+        user: {
+          avatar: getAvatarId(message.interaction.user.avatarUrl),
+          discriminator: '0000',
+          global_name: message.interaction.user.username,
+          username: message.interaction.user.username,
+          id: message.interaction.user.bot
+            ? getIdFromUrl(message.interaction.user.avatarUrl)
+            : message.interaction.user.id,
+          bot: message.interaction.user.bot
+        },
+
+        type: InteractionType.MessageComponent
+      } as APIMessageInteraction)
+    : undefined,
+
   mention_roles: [],
   pinned: false,
   tts: false,
